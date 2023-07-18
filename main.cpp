222dfae371312de3ccb74c7093c624a4b1542690
Mohamed Saad Ibn Seddik <ms.ibnseddik@gmail.com>
2023-07-18 08:10:18 -0400

    initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+  std::cout << "Hello, world!" << std::endl;
+
+  int x = 5;
+  int y = 7;
+  std::cout << "The sum of " << x << " and " << y << " is " << x + y << std::endl;
+
+  return 0;
+}
